d6d5c2fd103532e047e2c1d9ea36e4b7590e0ff4
AlbinHellman <steelalbin@gmail.com>
2021-01-14 18:38:55 +0100

    JavaScript Inlämningsuppgift1

--- Inlämningsuppgift-1/script.js
+++ Inlämningsuppgift-1/script.js
@@ -1,50 +1,32 @@
-let addd= [
-    {
-        id: '1',
-        title: 'Lägg till',
-        completed: false
-    },
-    
-]
+function onFormSubmit(){
+    var formData = readFormData();
+    insertNewRecord(formData);
 
-const form = document.querySelector('#todoForm');
-const input = document.querySelector("#name");
-const addBtn = document.querySelector('#läggtill');
-const output = document.querySelector('#add');
-
-
-const listadd = () => {
-    output.innerHTML = ''
-
-    addd.forEach(läggtill => {
-        output.innerHTML +=`<div id="${addd.id}" class="bg-primary p-3 d-flex justify-content-between mt-2">${addd.title}<button class="btn btn-dark px-3">X</button></div>`
-    })
 }
 
-addBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    if(input.value !== '') {
+function readFormData() {
+    var formData = {};
+    formData["name"] = document.getElementById("name").value;
+    formData["name2"] = document.getElementById("name2").value;
+    formData["email"] = document.getElementById("email").value;
+    return formData;
 
-        let add = {
-            id: Date.now().toString(),
-            title: input.value,
-            completed: false
-        }
-        addd.push(add);
-        listadd();
- 
-        input.value='';
-    } else {
-        input.classList.add('is-invalid');
-    }
-})
+}
 
-output.addEventListener('click', (e) => {
-    addd = addd.filter(add => add.id !== e.target.parentNode.id)
-})
 
-listadd();
-
+function insertNewRecord(data){
+    var table = document.getElementById("add").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.name;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.name2;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.email;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = `<a>Edit</a>
+                       <a>Delete</a>`;
+}
 
 
 
